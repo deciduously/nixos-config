03d83ca00d667e2fb813699659b2ef47fc3ac845
Ben Lovy <ben@deciduously.com>
2021-12-01 13:35:18 -0500

    Update, add tmpdir compat stuff

--- machines/common.nix
+++ machines/common.nix
@@ -123,6 +123,11 @@ inputs:
     vim
   ];
 
+	boot.cleanTmpDir = true;
+	systemd.tmpfiles.rules = [
+    "L+ /lib64/ld-linux-x86-64.so.2 - - - - ${pkgs.glibc}/lib64/ld-linux-x86-64.so.2"
+];
+
   #programs.bash.promptInit = ''
   #  PS1="$ "
   #'';
